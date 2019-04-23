4a683b8fa4e9e5b4b000bc44f80f10be0c966c8e
Cody Frank <cody.frank30@gmail.com>
2019-04-23 23:11:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,13 @@ def batch_badge_creator(array)
     "Hello, my name is #{name}."
   end
 end
+
+
+def assign_rooms(array)
+  new_array = []
+  array.each_with_index do |name, index|
+    room_number = index + 1
+      new_array << "Hello, #{name}! You'll be assigned to room #{room_number}!"
+  end
+  new_array
+end
